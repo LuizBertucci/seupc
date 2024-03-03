4d08fed5a579a28cfb0ee057ff6955508847eb25
edvillani <eduardo.carvalho_filho@syngenta.com>
2024-03-03 16:38:02 -0300

    migration files

---
+++ apps/core-api/src/migrations/20240303184104_AddBrandsTable.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('brands', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('name').notNullable();
+    table.string('status').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('brands');
+}
