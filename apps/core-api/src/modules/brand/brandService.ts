3ab6d6479bef6d437c5cdc912b215a2c05461851
edvillani <eduardo.carvalho_filho@syngenta.com>
2024-03-03 16:38:53 -0300

    brand service

---
+++ apps/core-api/src/modules/brand/brandService.ts
@@ -0,0 +1,71 @@
+import { StatusCodes } from 'http-status-codes';
+
+import { ResponseStatus, ServiceResponse } from '@common/models/serviceResponse';
+import { logger } from '@src/server';
+import { v4 as uuidv4 } from 'uuid';
+import { brandRepository } from '@modules/brand/brandRepository';
+import { Brand, CreateBrandRequest, GetBrandByIdResponse } from '@modules/brand/brandModel';
+
+const toDTO = (brand: Brand): GetBrandByIdResponse => ({
+  id: brand.id,
+  name: brand.name,
+  status: brand.status,
+  createdAt: brand.createdAt,
+  updatedAt: brand.updatedAt,
+});
+
+export const brandService = {
+  findAll: async (): Promise<ServiceResponse<GetBrandByIdResponse[] | null>> => {
+    try {
+      const brands = await brandRepository.findAllAsync();
+      return new ServiceResponse<GetBrandByIdResponse[]>(
+        ResponseStatus.Success,
+        'Brands encontradas',
+        brands.map(toDTO),
+        StatusCodes.OK
+      );
+    } catch (ex) {
+      const errorMessage = `Erro ao encontrar as brands: $${(ex as Error).message}`;
+      logger.error(errorMessage);
+      return new ServiceResponse(ResponseStatus.Failed, errorMessage, null, StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+  },
+
+  findById: async (id: string): Promise<ServiceResponse<GetBrandByIdResponse | null>> => {
+    try {
+      const brand = await brandRepository.findByIdAsync(id);
+      if (!brand) {
+        return new ServiceResponse(ResponseStatus.Failed, 'Nenhuma brand encontrada', null, StatusCodes.NOT_FOUND);
+      }
+
+      return new ServiceResponse<GetBrandByIdResponse>(
+        ResponseStatus.Success,
+        'Brand encontrada',
+        toDTO(brand),
+        StatusCodes.OK
+      );
+    } catch (ex) {
+      const errorMessage = `Erro ao encontrar a brand: $${(ex as Error).message}`;
+      logger.error(errorMessage);
+      return new ServiceResponse(ResponseStatus.Failed, errorMessage, null, StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+  },
+
+  createBrand: async (request: CreateBrandRequest): Promise<ServiceResponse<string | null>> => {
+    try {
+      const brand = await brandRepository.createBrand({
+        id: uuidv4(),
+        name: request.name,
+        status: request.status,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      return new ServiceResponse<string>(ResponseStatus.Success, 'Brand criada', brand.id, StatusCodes.CREATED);
+    } catch (ex) {
+      const errorMessage = `Erro ao criar a brand: ${(ex as Error).message}`;
+      logger.error(errorMessage);
+      return new ServiceResponse(ResponseStatus.Failed, errorMessage, null, StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+  },
+};
