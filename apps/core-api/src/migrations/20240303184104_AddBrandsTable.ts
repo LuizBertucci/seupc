38707f6e20cc29d662c3ef2cc3fefef1ed59008c
edvillani <eduardo.carvalho_filho@syngenta.com>
2024-03-07 15:50:38 -0300

    removing status from migration

--- apps/core-api/src/migrations/20240303184104_AddBrandsTable.ts
+++ apps/core-api/src/migrations/20240303184104_AddBrandsTable.ts
@@ -4,7 +4,6 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('brands', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
     table.string('name').notNullable();
-    table.string('status').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
