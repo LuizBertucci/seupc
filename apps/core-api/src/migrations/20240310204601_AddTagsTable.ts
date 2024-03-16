6a3ea693bdc228cd2cc0c0d2746e903a20fe4662
edvillani <eduardo.carvalho_filho@syngenta.com>
2024-03-15 21:23:39 -0300

    fixing migrate file

--- apps/core-api/src/migrations/20240310204601_AddTagsTable.ts
+++ apps/core-api/src/migrations/20240310204601_AddTagsTable.ts
@@ -1,10 +1,9 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('tags', function (table) {
     table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
-    table.enum('category', ['Games','Programs', 'Courses']).notNullable();
+    table.enum('category', ['Games', 'Programs', 'Courses']).notNullable();
     table.string('name', 255).notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
@@ -15,7 +14,7 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('tag_id').primary();
 
     table.foreign('part_id').references('id').inTable('parts');
-    table.foreign('tag_id').references('id').inTable('tag');
+    table.foreign('tag_id').references('id').inTable('tags');
   });
 }
 
