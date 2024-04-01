4f5a5825c80f8a0e8aa44179acbfc5e083a17727
Igor Oliveira <igortheodoro12@gmail.com>
2024-04-01 19:50:34 -0300

    feat(tables-admin): modifica URL

--- apps/client-app/src/api/config.ts
+++ apps/client-app/src/api/config.ts
@@ -1,6 +1,6 @@
-import axios from "axios"
+import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: "http://localhost:8080",
-	headers: { Accept: "application/json" },
+  baseURL: 'https://seupc-core-api.azurewebsites.net/',
+  headers: { Accept: 'application/json' },
 });
