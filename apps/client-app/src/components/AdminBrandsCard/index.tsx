9708ed1ba76009806ee6616d714fd876ab40ade5
Pedro Barros <pedrobars7a@gmail.com>
2024-04-25 08:03:02 -0300

    feature: little improvements on "create" modals

--- apps/client-app/src/components/AdminBrandsCard/index.tsx
+++ apps/client-app/src/components/AdminBrandsCard/index.tsx
@@ -1,32 +1,40 @@
-"use client"
+'use client';
 
-import { SimpleTable } from "@/components/ui/simpleTable";
-import { useToast } from "@/components/ui/use-toast";
-import { useEffect, useState } from "react";
-import OptionsTable from "./components/Options";
-import { requestGetMarcas } from "./hooks/request";
-import { useMarcasStore } from "./storage";
+import { useEffect, useState } from 'react';
+
+import { SimpleTable } from '@/components/ui/simpleTable';
+import { useToast } from '@/components/ui/use-toast';
+
+import OptionsTable from './components/Options';
+import { requestGetMarcas } from './hooks/request';
+import { useMarcasStore } from './storage';
 
 export default function AdminBrandsCard() {
-  const { dataTable, columns } = useMarcasStore((state) => state.dados)
-  const [rowSelection, setRowSelection] = useState({})
-  const { toast } = useToast()
+  const { dataTable, columns } = useMarcasStore((state) => state.dados);
+  const [rowSelection, setRowSelection] = useState({});
+  const { toast } = useToast();
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     const getData = async () => {
-      await requestGetMarcas()
-      toast({ title: "Marcas encontradas com sucesso!" })
-    }
+      await requestGetMarcas();
+      toast({ title: 'Marcas encontradas com sucesso!' });
+    };
 
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
-    <>
-      <div className="w-1/2" >
-        <SimpleTable title="Marcas" setRowSelection={setRowSelection} rowSelection={rowSelection} columns={columns || []} data={dataTable || []} rightMenu={<OptionsTable setRowSelection={setRowSelection} rowSelection={rowSelection} />} className="w-full" />
-      </div>
-    </>
-  )
+    <div className="w-1/2">
+      <SimpleTable
+        title="Marcas"
+        setRowSelection={setRowSelection}
+        rowSelection={rowSelection}
+        columns={columns || []}
+        data={dataTable || []}
+        rightMenu={<OptionsTable setRowSelection={setRowSelection} rowSelection={rowSelection} />}
+        className="w-full"
+      />
+    </div>
+  );
 }
--- apps/client-app/src/components/AdminCategoriesTagsCard/components/Modal/create.tsx
+++ apps/client-app/src/components/AdminCategoriesTagsCard/components/Modal/create.tsx
@@ -1,41 +1,63 @@
-import { Button } from '@/components/ui/button'
-import { DialogHeader, ModalContext } from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { faAdd, faSave } from '@fortawesome/free-solid-svg-icons'
-import { useForm, SubmitHandler } from "react-hook-form"
-import { CategoryTags } from "@/types/parts"
-import { requestAddCategoriesTags, requestEditCategoriesTags } from '../../hooks/request'
-import { useToast } from '@/components/ui/use-toast'
-import { useContext } from 'react'
+import { faAdd, faSave } from '@fortawesome/free-solid-svg-icons';
+import { useContext } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-export default function CreateCategoriesTags({ edit, editValues, editIndex }: { edit?: boolean, editValues?: CategoryTags, editIndex?: number }) {
-  const { toast } = useToast()
-  const { setIsOpen } = useContext(ModalContext)
+import { Button } from '@/components/ui/button';
+import { DialogHeader, ModalContext } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/use-toast';
+import { CategoryTags } from '@/types/parts';
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-      } = useForm<CategoryTags>({ defaultValues: edit ? editValues : {}})
+import { requestAddCategoriesTags, requestEditCategoriesTags } from '../../hooks/request';
 
-      const onSubmit: SubmitHandler<CategoryTags> = async (data: CategoryTags) => {
-       edit ? await requestEditCategoriesTags(data, editIndex) : await requestAddCategoriesTags(data)
-      
-        toast({ title: `Categoria ${edit ? "editada" : "criada"} com sucesso!`})
-        setIsOpen(false)
-      } 
+interface ICreateCategoriesTagsProps {
+  edit?: boolean;
+  editValues?: CategoryTags;
+  editIndex?: number;
+}
+
+function CreateCategoriesTags({ edit, editValues, editIndex }: ICreateCategoriesTagsProps) {
+  const { toast } = useToast();
+  const { setIsOpen } = useContext(ModalContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CategoryTags>({ defaultValues: edit ? editValues : {} });
+
+  const onSubmit: SubmitHandler<CategoryTags> = async (data: CategoryTags) => {
+    if (edit) {
+      await requestEditCategoriesTags(data, editIndex);
+    } else {
+      await requestAddCategoriesTags(data);
+    }
+
+    toast({ title: `Categoria ${edit ? 'editada' : 'criada'} com sucesso!` });
+    setIsOpen(false);
+  };
 
   return (
     <>
-    <DialogHeader>
-        <h2>{edit ? "Editar" : "Criar"} Categoria Tag</h2>
-    </DialogHeader>
-    <form className='flex flex-col space-y-4 justify-center items-center w-full ' onSubmit={handleSubmit(onSubmit)} >
+      <DialogHeader>
+        <h2>{edit ? 'Editar' : 'Criar'} Categoria Tag</h2>
+      </DialogHeader>
+      <form className="flex flex-col space-y-4 justify-center items-center w-full " onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          formError={errors?.category?.message}
+          {...register('category', {
+            required: 'Nome para a categoria obrigatório',
+            maxLength: { value: 100, message: 'Tamanho máximo do nome ultrapassado' },
+          })}
+          placeholder="Nome da Categoria"
+        />
 
-    <Input formError={errors?.category?.message} {...register("category", { required: "Nome para a categoria obrigatório", maxLength: { value: 100, message: "Tamanho máximo do nome ultrapassado" } }) } placeholder='Nome da Categoria' />
-
-    <Button type='submit' className='self-end' closeModal errors={errors} icon={edit ? faSave : faAdd} >{edit ? "Salvar" : "Criar"}</Button>
-    </form>
+        <Button type="submit" className="self-end" closeModal errors={errors} icon={edit ? faSave : faAdd}>
+          {edit ? 'Salvar' : 'Criar'}
+        </Button>
+      </form>
     </>
-  )
+  );
 }
+
+export default CreateCategoriesTags;
