8ff86feec26cd3555bdb28104ecae3c315ecd11e
Pedro Barros <pedrobars7a@gmail.com>
2024-05-09 20:18:05 -0300

    feature(AdminNotebooks): expand card

--- apps/client-app/src/components/AdminNotebooks/AdminNotebooksForm.tsx
+++ apps/client-app/src/components/AdminNotebooks/AdminNotebooksForm.tsx
@@ -36,31 +36,20 @@ export default function AdminNotebooksForm({
   return (
     <>
       <DialogHeader>
-        <h2>{edit ? 'Editar' : 'Criar'} Notebook</h2>
+        <h2>Infos</h2>
       </DialogHeader>
       <form className="flex flex-col space-y-4 justify-center items-center w-full " onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          formError={errors?.name?.message}
-          {...register('name', {
-            required: 'Nome para o notebook obrigatório',
-            maxLength: { value: 100, message: 'Tamanho máximo do nome ultrapassado' },
-          })}
-          placeholder="Nome do Notebook"
-        />
+        <div className="w-full">
+          <Input
+            formError={errors?.name?.message}
+            {...register('name', {
+              required: 'Nome para o notebook obrigatório',
+              maxLength: { value: 100, message: 'Tamanho máximo do nome ultrapassado' },
+            })}
+            placeholder="Nome do Notebook"
+          />
+        </div>
 
-        <Input
-          formError={errors?.brand?.message}
-          {...register('brand', {
-            required: 'Nome para a marca do notebook é obrigatória',
-            maxLength: { value: 100, message: 'Tamanho máximo do nome ultrapassado' },
-          })}
-          placeholder="Nome da marca"
-        />
-
-        <Input placeholder="Cor" {...register('color')} />
-        <Input placeholder="Tamanho da Tela" {...register('screen_size')} />
-        <Input placeholder="Resolução da Tela" {...register('screen_resolution')} />
-        <Input placeholder="Bateria" {...register('battery')} />
         <Controller
           name="has_numeric_keypad"
           control={control}
@@ -75,9 +64,39 @@ export default function AdminNotebooksForm({
             />
           )}
         />
-        <Input placeholder="Sistema Operacional" {...register('operating_system')} />
+        <div className="flex gap-3 w-full flex-1">
+          <div className="flex flex-col">
+            <p>Marca</p>
+            <Input
+              formError={errors?.brand?.message}
+              {...register('brand', {
+                required: 'Nome para a marca do notebook é obrigatória',
+                maxLength: { value: 100, message: 'Tamanho máximo do nome ultrapassado' },
+              })}
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <p>Resolução da Tela</p>
+            <Input {...register('screen_resolution')} />
+          </div>
+          <div className="flex flex-col">
+            <p>Tamanho da Tela</p>
+            <Input {...register('screen_size')} />
+          </div>
+          <div className="flex flex-col">
+            <p>S.O</p>
+            <Input {...register('operating_system')} />
+          </div>
+          <div className="flex flex-col">
+            <p>Cor</p>
+            <Input {...register('color')} />
+          </div>
+        </div>
+
+        {/* <Input placeholder="Bateria" {...register('battery')} />
         <Input placeholder="ID do Fabricante" {...register('manufacturer_id')} />
-        <Input placeholder="Peso" {...register('weight')} />
+        <Input placeholder="Peso" {...register('weight')} /> */}
 
         <Button type="submit" className="self-end" closeModal errors={errors} icon={edit ? faSave : faAdd}>
           {edit ? 'Salvar' : 'Criar'}
