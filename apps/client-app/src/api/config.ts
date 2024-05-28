0c55e93b0da5bdc7e10e0c59ae9fcab76fecf2f8
Pedro Barros <pedrobars7a@gmail.com>
2024-05-28 15:03:59 -0300

    feature(tag_parts): remove leftovers

--- apps/client-app/src/api/config.ts
+++ apps/client-app/src/api/config.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'https://seupc-core-api.azurewebsites.net/',
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://seupc-core-api.azurewebsites.net/',
   headers: { Accept: 'application/json' },
 });
